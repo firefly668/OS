c5d461bfab87ba2d69fb1005e4cb1b1bae13b73b
wy672515431 <672515431@qq.com>
2020-11-01 13:44:05 +0800

    fix some bugs
    1

---
+++ pintos-anon-b9454eb/src/userprog/syscall.c
@@ -0,0 +1,83 @@
+#include "userprog/syscall.h"
+#include <stdio.h>
+#include <syscall-nr.h>
+#include "threads/interrupt.h"
+#include "threads/thread.h"
+#include "threads/synch.h"
+#include "threads/vaddr.h"
+static void syscall_handler (struct intr_frame *);
+struct lock filesystem_lock;
+void
+syscall_init (void) 
+{
+  lock_init(&filesystem_lock);
+  intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
+}
+
+static void
+syscall_handler (struct intr_frame *f UNUSED) 
+{
+  is_valid_addr(f->esp);
+  if(*(int *)f->esp == SYS_WRITE){
+    int parameters[10];
+    get_parameters(f,parameters,3);
+    write(parameters[0],(void *)parameters[1],(unsigned) parameters[2]);
+  }
+  else if(*(int *)f->esp == SYS_EXIT){
+    int parameters[10];
+    get_parameters(f,parameters,1);
+    exit(parameters[0]);
+  }
+}
+void
+is_valid_addr (const void *addr)
+{
+  if (addr == NULL || !is_user_vaddr (addr) || pagedir_get_page (thread_current ()->pagedir, addr) == NULL)
+    {
+      if (lock_held_by_current_thread (&filesystem_lock))
+        lock_release (&filesystem_lock);
+      exit (-1);
+    }
+}
+void
+is_valid_buffer (void *buffer, unsigned size)
+{
+  char *temp = (char *)buffer;
+  is_valid_addr ((const char *)temp);
+  temp+=size;
+  is_valid_addr ((const char *)temp);
+}
+int write(int fd,const void *buffer, unsigned size)
+{
+    lock_acquire(&filesystem_lock);
+    /* Try writing to fd 0 (stdin),  which may just fail or terminate the process with -1 exit code. */
+    if(fd == 0){
+      lock_release(&filesystem_lock);
+      exit(-1);
+    }
+    if(fd == 1){
+       /*write all of buﬀer in one call to putbuf(), */
+       putbuf((const char *)buffer,(size_t)size);
+       lock_release(&filesystem_lock);
+       return size;
+    }
+    exit(-1);
+}
+int exit(int stauts){
+  struct thread* t = thread_current();
+  t->ret = stauts;
+  printf("%s: exit(%d)\n", t->name, t->ret);
+  thread_exit();
+}
+/*获取压到栈上的系统调用的参数
+第一个参数为中断栈帧，第二个参数为存放系统调用的参数(不能使用void*)，第三个参数为该系统调用有几个参数*/
+void get_parameters(struct intr_frame *f,int *parameters,int len)
+{
+  void *tem;
+  for(int i=0;i<len;i++){
+    tem = (int*)f->esp+i+1;
+    is_valid_addr(tem);
+    parameters[i] = *(int *)tem;
+    
+  }
+}
