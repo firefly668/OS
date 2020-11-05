b8d61cec299b48dd150c5f20275aa381ae05be54
firefly668 <49812001+firefly668@users.noreply.github.com>
2020-11-05 16:06:03 +0800

    80/80 pass

--- pintos/src/userprog/exception.c
+++ pintos/src/userprog/exception.c
@@ -149,7 +149,9 @@ page_fault (struct intr_frame *f)
   not_present = (f->error_code & PF_P) == 0;
   write = (f->error_code & PF_W) != 0;
   user = (f->error_code & PF_U) != 0;
-
+  if(user){
+     exit(-1);
+  }	
   /* To implement virtual memory, delete the rest of the function
      body, and replace it with code that brings in the page to
      which fault_addr refers. */
@@ -160,4 +162,5 @@ page_fault (struct intr_frame *f)
           user ? "user" : "kernel");
   kill (f);
 }
+
 
